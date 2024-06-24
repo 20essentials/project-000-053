7d8a113c4ca84cb926e73d0402fb5025b4b33a81
20essentials <s0l0estudi4r@gmail.com>
2024-06-24 16:55:47 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,9 @@
+console.group("REFERENCE");
+console.log(
+  "image 1",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png"
+);
+console.log(
+  "image 2",
+  "ttps://c0.klipartz.com/pngpicture/1018/76/gratis-png-fc-barcelona-futbolista-la-liga-pelota.png"
+);
